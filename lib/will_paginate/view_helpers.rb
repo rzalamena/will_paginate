31cc5e923e854b9e5cacd49155637e2271a688e1
Rafael Zalamena <rzalamena@gmail.com>
2016-02-18 22:51:57 -0200

    Always show will_paginate
    Accept new option to hide will_paginate when we got just one page.

--- lib/will_paginate/view_helpers.rb
+++ lib/will_paginate/view_helpers.rb
@@ -70,7 +70,7 @@ module WillPaginate
     #
     def will_paginate(collection, options = {})
       # early exit if there is nothing to render
-      return nil unless collection.total_pages > 1
+      return nil unless collection.total_pages > 1 or not options[:no_single_page]
 
       options = WillPaginate::ViewHelpers.pagination_options.merge(options)
 
